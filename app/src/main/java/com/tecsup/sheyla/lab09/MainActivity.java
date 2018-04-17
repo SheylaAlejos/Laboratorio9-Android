da47c82c91c326768767ac8a972813e523087714
SheylaAlejos <sheyla.alejos@tecsup.edu.pe>
2018-04-16 23:23:30 -0500

    Initial commit

---
+++ app/src/main/java/com/tecsup/sheyla/lab09/MainActivity.java
@@ -0,0 +1,62 @@
+package com.tecsup.sheyla.lab09;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText codeInput;
+    private EditText fullnameInput;
+    private EditText amountInput;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        codeInput = (EditText)findViewById(R.id.codeInput);
+        fullnameInput = (EditText)findViewById(R.id.fullnameInput);
+        amountInput = (EditText)findViewById(R.id.amountInput);
+    }
+
+    public void sendExplicit(View view){
+
+        String code = codeInput.getText().toString();
+        String fullname = fullnameInput.getText().toString();
+        String amount = amountInput.getText().toString();
+
+        if("".equals(code) || "".equals(fullname) || "".equals(amount)){
+            Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("code", Integer.parseInt(code));
+        intent.putExtra("fullname", fullname);
+        intent.putExtra("amount", Double.parseDouble(amount));
+        startActivity(intent);
+    }
+
+    public void sendImplicit(View view){
+
+        String code = codeInput.getText().toString();
+        String fullname = fullnameInput.getText().toString();
+        String amount = amountInput.getText().toString();
+
+        if("".equals(code) || "".equals(fullname) || "".equals(amount)){
+            Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, "code: "+code+"\nfullname: "+fullname+"\namount: "+amount);
+        intent.setType("text/plain");
+        startActivity(intent);
+    }
+
+}
